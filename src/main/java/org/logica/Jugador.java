0d2f01a667465a8cfc1ddbd8964305c8ac196cc6
Javiera D <jd2025@udec.cl>
2026-06-23 12:07:58 -0400

    fix: Correcion del tipo de clase de la Clase Jugador (Abstracta a concreta)

--- src/main/java/org/logica/Jugador.java
+++ src/main/java/org/logica/Jugador.java
@@ -1,6 +1,6 @@
 package org.logica;
 
-public abstract class Jugador extends Participante {
+public class Jugador extends Participante {
 
     public Jugador(String id, String nombre, String contacto) {
         super(id, nombre, contacto);
