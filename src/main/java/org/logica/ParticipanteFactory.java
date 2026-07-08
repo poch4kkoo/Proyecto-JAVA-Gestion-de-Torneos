0a1861b5c2def8fcf7fdc614b529a0853df7f1f3
Javiera D <jd2025@udec.cl>
2026-07-08 19:09:11 -0400

    docs: Documentacion de los metodos de la Clase ParticipanteFactory

--- src/main/java/org/logica/ParticipanteFactory.java
+++ src/main/java/org/logica/ParticipanteFactory.java
@@ -1,8 +1,19 @@
 package org.logica;
+
+/**
+ * Clase de utilidad que implementa el patrón de diseño Factory.
+ * Se encarga de centralizar y desacoplar la creación de instancias concretas de Participantes.
+ */
 public class ParticipanteFactory {
 
     /**
-     * Fabrica un participante dependiendo del tipo seleccionado en la interfaz.
+     * Fabrica y devuelve una instacia especifica de un participante dependiendo del tio seleccionado por le usuario.
+     * @param tipo Naturaleza del competidor ("Jugador" o "Equipo").
+     * @param id Identificador unico del nuevo participante.
+     * @param nombre Nombre asignado al nuevo participante.
+     * @param contacto Informacion de contacto del nuevo participante.
+     * @return Una instacia concreta derivada de la clase abstracta Participante.
+     * @throws IllegalArgumentException Si el parametro tipo no coincide con ninguna subclase valida ("Jugador" o "Equipo").
      */
     public static Participante crearParticipante(String tipo, String id, String nombre, String contacto) {
         if (tipo.equalsIgnoreCase("Jugador")) {
