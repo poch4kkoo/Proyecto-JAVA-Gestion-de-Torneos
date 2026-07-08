6910f38dc2194158ffd839fda4ce4a6d8ec23517
Javiera D <jd2025@udec.cl>
2026-07-08 19:33:08 -0400

    docs: Documentacion de la Interfaz Observer

--- src/main/java/org/logica/Observer.java
+++ src/main/java/org/logica/Observer.java
@@ -1,6 +1,15 @@
 package org.logica;
 
+/**
+ * Interfaz que define el contrato básico para la implementación del patrón de diseño Observer.
+ * Permite establecer un mecanismo de suscripción para que cualquier componente del sistema sea
+ * notificado de manera automatica ante cambios en el torneo.
+ */
 public interface Observer {
 
+    /**
+     * Metodo de actualizacion que es invocado de manera sincrona por el sujeto observado (GestorTorneo).
+     * cuando ocurre una modificacion critica en los datos.
+     */
     void actualizar();
 }
