c3fe5023d79ce1ef9b5b23831124c9caca76d5a4
Javiera D <jd2025@udec.cl>
2026-07-08 19:35:06 -0400

    docs: Documentacion de Enum FormatoTorneo

--- src/main/java/org/logica/FormatoTorneo.java
+++ src/main/java/org/logica/FormatoTorneo.java
@@ -1,7 +1,19 @@
 package org.logica;
 
+/**
+ * Enumeración que define las modalidades o estructuras de competición admitidas por el sistema.
+ */
 public enum FormatoTorneo {
+
+    //Formato de muerte súbita o eliminación simple.
+    //El participante queda descalificado inmediatamente despues de perder.
     ELIMINATORIA_DIRECTA,
+
+    //Formato de eliminacion doble, se generan dos cuadros.
+    //Una vez pierde un participante no queda eliminado de meanera inmediata, sino que
+    //baja a el Cuadro de los Perdedores.
     ELIMINATORIA_DOBLE,
+
+    //Formato de liga tradicional o "todos contra todos"
     LIGA_SIMPLE
 }
